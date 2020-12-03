20178de6b900eeb8e60b8e767ff3061881ec12f0
Will Dane <enadlliw@gmail.com>
2020-12-03 17:40:07 -0500

    started work on server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,1 +1,21 @@
-const express = require("express");
+var express = require("express");
+var db = require("./db.json");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  return res.json(notes);
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
